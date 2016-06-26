571289162c515aecb0e9b263dbb24ac29d5c2961
Vipin Dwivedi <vipin20427@gmail.com>
2016-06-26 18:06:39 +0530

    initializing for static files

--- config/configure.js
+++ config/configure.js
@@ -3,6 +3,9 @@ var bodyParser = require ('body-parser');
 var cookieParser = require('cookie-parser');
 var routes = require('./routes');
 var handlebars = require('express-handlebars');
+var errorHandler = require('errorHandler');
+var path = require('path');
+var express = require('express');
 
 module.exports = function (app){
 
@@ -27,6 +30,13 @@ module.exports = function (app){
 	}).engine);
 	app.set('view engine', 'handlebars');
 
+	//for static rendering of the files.
+	app.use('/public/', express.static(path.join(__dirname, '../public')));
+
+	//errorhandler setup
+	if ( 'development' == app.get('env')){
+		app.use(errorHandler());
+	}
 
 	return app;
 };
