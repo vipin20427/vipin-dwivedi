2c2c8322c235dae5404943a81470cf70fe7afa1b
Vipin Dwivedi <vipin20427@gmail.com>
2016-06-26 17:10:28 +0530

    initialising server.js configurations

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+//var config = require ('./config/configure');
+
+
+app.set ('port', process.env.PORT || 3000);
+app.set ( 'views', __dirname + '/views');
+//app = config (app);
+
+app.listen (app.get ('port'), function (){
+	console.log ("Server started at port " + app.get('port'));
+});
