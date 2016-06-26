1bb409ae5a5e3f1cca47cd61fc0c3c869c2a5fa8
Vipin Dwivedi <vipin20427@gmail.com>
2016-06-26 17:39:51 +0530

    Initializing the Router

--- config/configure.js
+++ config/configure.js
@@ -1,11 +1,15 @@
 var morgan = require ('morgan');
 var bodyParser = require ('body-parser');
 var cookieParser = require('cookie-parser');
+var routes = require('./routes');
 
 module.exports = function (app){
 
 	//enable logging using morgan
 	app.use(morgan('combined'));
+
+	//initialize the routes
+	routes(app);
 
 	//enable body parsing
 	app.use(bodyParser.urlencoded({extended : true}));
