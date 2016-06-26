d2fb13f83c667bbb4281f25cbfe49a2171010460
Vipin Dwivedi <vipin20427@gmail.com>
2016-06-26 17:31:44 +0530

    initialising body-parser module

--- config/configure.js
+++ config/configure.js
@@ -1,9 +1,14 @@
 var morgan = require ('morgan');
-
+var bodyParser = require ('body-parser');
 
 module.exports = function (app){
 
+	//enable logging using morgan
 	app.use(morgan('combined'));
+
+	//enable body parsing
+	app.use(bodyParser.urlencoded({extended : true}));
+	app.use(bodyParser.json());
 
 
 	return app;
